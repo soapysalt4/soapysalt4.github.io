a0110ea2a078e16c3410ba43711790431706eda6
Evergreen114 <orrysander@gmail.com>
2026-02-02 09:52:08 -0800

    Update tracking-script.js

--- tracking-script.js
+++ tracking-script.js
@@ -1,2 +1,3 @@
-window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
-cronitor('config', { clientKey: 'b72461a152926ec0d8e6d87824007208' });
+async src="https://rum.cronitor.io/script.js"></script>
+    window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
+    cronitor('config', { clientKey: 'b72461a152926ec0d8e6d87824007208' });
