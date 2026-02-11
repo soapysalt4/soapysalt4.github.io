50670ff382110f3880e0ce7839c83398584a5c40
VexaCloud <orrysander@gmail.com>
2026-02-11 13:23:50 -0800

    Update login-full-directory.js

--- login-full-directory.js
+++ login-full-directory.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    //OsonEsMuySkib, Jamie Hessler, Givini Shmeltser, PP Pisston, Grant Hessler, Oscar Blacksmith, Gram Highskibidi, Arty Faggy, Jordan Es Hammy, Holden Is Blacky!
-    const ADMIN_CODES = new Set(["893880", "199032", "296260", "127074", "796447", "412654", "196088", "685666", "180572", "263879"]);
+    //OsonEsMuySkib, Jamie Hessler, Givini Shmeltser, PP Pisston, Grant Hessler, Oscar Blacksmith, Gram Highskibidi, Arty Faggy, Jordan Es Hammy, Holden Is Blacky, Jaredy Jaspery!
+    const ADMIN_CODES = new Set(["893880", "199032", "296260", "127074", "796447", "412654", "196088", "685666", "180572", "263879", "152160"]);
     const STORAGE_KEY = "admin_authenticated";
 
     // Check if already authenticated
