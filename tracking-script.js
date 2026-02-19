0f1c96ec060d25ce95024cceabc31d11854083c0
VexaCloud <orrysander@gmail.com>
2026-02-19 15:31:51 -0800

    Update tracking-script.js

--- tracking-script.js
+++ tracking-script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
 async src="https://rum.cronitor.io/script.js"></script>
     window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
     cronitor('config', { clientKey: 'b72461a152926ec0d8e6d87824007208' });
+});
