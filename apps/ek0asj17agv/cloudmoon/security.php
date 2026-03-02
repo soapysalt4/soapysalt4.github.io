03dece2429fabff1c59439a0ada514d44c6738b3
VexaCloud <orrysander@gmail.com>
2026-03-01 17:42:55 -0800

    Update and rename security.html to security.php

---
+++ apps/ek0asj17agv/cloudmoon/security.php
@@ -0,0 +1,10 @@
+<?php
+if (!isset($_GET['key']) || $_GET['key'] !== 'ag91jagg1') {
+    http_response_code(403);
+    exit;
+}
+
+header('Content-Type: text/html; charset=UTF-8');
+?>
+
+<!-- URL:https://d1srn6k5rwdbiy.cloudfront.net/ -->
