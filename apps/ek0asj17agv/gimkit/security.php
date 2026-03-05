083d1dce246369ed702de313f0cb95b1d6d231d4
VexaCloud <orrysander@gmail.com>
2026-03-04 20:16:46 -0800

    Create security.php

---
+++ apps/ek0asj17agv/gimkit/security.php
@@ -0,0 +1,10 @@
+<?php
+if (!isset($_GET['key']) || $_GET['key'] !== 'ag91jagg1') {
+    http_response_code(403);
+    exit;
+}
+
+header('Content-Type: text/html; charset=UTF-8');
+?>
+
+<!-- URL:https://gimmicking-worker.orson-sander.workers.dev -->
